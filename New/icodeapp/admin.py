71265fff4505f3c673906b798e4e338b580b24d7
NasrCoding <101987719+NasrCoding@users.noreply.github.com>
2022-03-21 21:53:02 +0500

    Add files via upload

---
+++ New/icodeapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Category, Post
+
+# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category
+)
+
+
+
